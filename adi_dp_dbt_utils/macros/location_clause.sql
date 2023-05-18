c66a2a2ea269099efe4d5b085e1a74499ce8f8c1
zhaoyong <zhaoyoong@gmail.com>
2023-05-18 10:50:24 +0800

    added more base macros

--- adi_dp_dbt_utils/macros/location_clause.sql
+++ adi_dp_dbt_utils/macros/location_clause.sql
@@ -1,39 +1,5 @@
 {% macro location_clause() %}
 
-  {%- set default_schema = target.schema -%}
-  {%- set location_root = config.get('location_root', validator=validation.any[basestring]) -%}
-  {%- set identifier = model['alias'] -%}
-  {%- set fqn = model['fqn'] -%}
-
-  {%- set catalog = model['database'] -%}
-  {%- set model_layer = fqn[1] -%}
-
-  {%- if location_root is not none %}
-
-    {%- set location = location_root ~ '/' ~ identifier -%}
-
-  {%- else -%}
-
-    {%- set s3_bucket = 'aidia-' ~ catalog|replace("_", "-") -%}
-    {%- set db_name = fqn[2] -%}
-
-    {%- if model_layer == 'clean' or model_layer == 'inst' -%}
-
-      {%- if target_schema == 'prod' -%}
-
-        {%- set location = 's3://' ~ s3_bucket ~ '/' ~ model_layer ~ '/' ~ db_name ~ '/' ~ identifier -%}
-
-      {%- else -%}
-
-        {%- set location = 's3://' ~ s3_bucket ~ '/' ~ model_layer ~ '/' ~ db_name ~ '_' ~ default_schema ~ '/' ~ identifier -%}
-
-      {%- endif -%}
-
-    {%- endif %}
-
-  {%- endif %}
-
-  {{ log('Writing the data files to ' ~ location, True) }}
-  location '{{ location }}'
+  location '{{ construct_location() }}'
 
 {%- endmacro -%}
